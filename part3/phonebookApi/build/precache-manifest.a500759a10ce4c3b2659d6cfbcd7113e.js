dd1fb67341fc80418d2c83ecf51a6ab55f3b00b4
Julian <jmpaviotti@gmail.com>
2020-08-04 16:50:19 -0300

    Add production build

---
+++ part3/phonebookApi/build/precache-manifest.a500759a10ce4c3b2659d6cfbcd7113e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ec4852918732a1c20e99a0582c6072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c1105f33ed9a7d4599",
+    "url": "/static/js/main.5e253017.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
